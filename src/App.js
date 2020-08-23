328de988ba8ea0f95dbdb9b26cd8141c4879b135
guard-skills <64185226+guard-skills@users.noreply.github.com>
2020-08-23 19:26:47 +1200

    Mobile News App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import View from './View'
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+import NewsCard from './newsCard';
+import Featured from './Featured';
+import AddArticle from './AddArticle';
+import UpdateArticle from './UpdateArticle';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      activeView:'home',
+      news:[
+        {
+          id: 1,
+          title: "Title",
+          author: "John Smith",
+          category: "World",
+          source: "Source",
+          timestamp: "2020 21/08 15:00",
+        },
+      ]
+    };
+  }
+
+  setActiveView = (view) => {
+    this.setState({activeView:view})
+  }
+
+  render(){
+    return (
+      <div className="app">
+        <View viewName="home" activeView={this.state.activeView} className="home">
+          <header>
+              <div className="logo"><span>dot</span>news</div>
+              <div className="header-right">
+                  <div>Add Article</div>
+                  <i className="far fa-plus-square" onClick={
+                    ()=>this.setActiveView('add-article')
+                  }></i>
+              </div>
+          </header>
+
+          <div className="main">
+            <Featured />
+  
+            <Tabs defaultActiveKey="world" id="tabs-home" className="nav nav-tabs">
+                  <Tab eventKey="world" title="World" className="nav-item">
+                    <div className="container">
+                      <div className="news-cards">
+                        <NewsCard setActiveView={this.setActiveView}/>
+                        <NewsCard setActiveView={this.setActiveView}/>
+                        <NewsCard setActiveView={this.setActiveView}/>
+                      </div>
+                  </div>
+                  </Tab>
+                  <Tab eventKey="nation" title="Nation" className="nav-item">
+                    <div className="container">
+                      <div className="news-cards">
+                        <NewsCard setActiveView={this.setActiveView}/>
+                        <NewsCard setActiveView={this.setActiveView}/>
+                        <NewsCard setActiveView={this.setActiveView}/>
+                      </div>
+                    </div>
+                  </Tab>
+                  <Tab eventKey="business" title="Business" className="nav-item">
+                    <div className="container">
+                        <div className="news-cards">
+                          <NewsCard setActiveView={this.setActiveView}/>
+                          <NewsCard setActiveView={this.setActiveView}/>
+                          <NewsCard setActiveView={this.setActiveView}/>
+                        </div>
+                      </div>
+                  </Tab>
+              </Tabs>
+  
+              
+  
+          </div>
+        </View>
+
+        <View viewName="add-article" activeView={this.state.activeView} className="add-article">
+          <AddArticle setActiveView={this.setActiveView}/>
+        </View>
+
+        <View viewName="update-article" activeView={this.state.activeView} className="update-article">
+          <UpdateArticle setActiveView={this.setActiveView} />
+        </View>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
